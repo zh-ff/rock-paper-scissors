0ce3361e24a4e1d555992740d77ca38c0ba5950c
zh-ff <40149294+zh-ff@users.noreply.github.com>
2023-11-22 16:20:52 +0300

    feat: add game functions

--- scripts/script.js
+++ scripts/script.js
@@ -17,4 +17,45 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == "scissors") {
+        if (computerSelection == "paper") {
+            return "You Win! Scissors beat paper!";
+        } else if (computerSelection == "rock") {
+            return "You Lose! Rock beats scissors!";
+        } else {
+            return "Draw!";
+        }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            return "You Win! Paper beats rock!";
+        } else if (computerSelection == "scissors") {
+            return "You Lose! Scissors beat paper!";
+        } else {
+            return "Draw!";
+        }
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "scissors") {
+            return "You Win! Rock beats scissors!";
+        } else if (computerSelection == "paper") {
+            return "You Lose! Paper beats rock!";
+        } else {
+            return "Draw!";
+        }
+    } else {
+        "Your input is incorrect!";
+    }
+}
+
+function game() {
+    let playerSelection;
+    let computerSelection;
+    for (let i = 1; i <= 5; i++) {
+        playerSelection = prompt("enter rock, paper or scissors").toLowerCase();
+        computerSelection = getComputerChoice().toLowerCase();
+        console.log(`Round ${i}\nPlayer choice: ${playerSelection}\nComputer choice: ${computerSelection}`)
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+game();
